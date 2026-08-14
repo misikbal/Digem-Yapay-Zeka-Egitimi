6c4055f3908e09441cd9e84aed551c3e03e79306
misikbal <ikbal.sirdas@gmail.com>
2026-08-14 21:30:14 +0300

    hata yönetimi

--- 17-Kargo Yönetimi/proje.py
+++ 17-Kargo Yönetimi/proje.py
@@ -20,8 +20,71 @@ def verileri_yukle():
         return {"kargolar":{}}
 
 
-def verileri_kaydet(data):
-    pass
+def verileri_kaydet(yeni_veri):
+    try:
+        with open(dosya_adi,"w",encoding="utf-8") as var_olan_veriler:
+            json.dump(yeni_veri, var_olan_veriler, indent=4, ensure_ascii=False)
+
+    except Exception as error:
+        print(f"Dosya yazım hatası oluştu Hata Kodu:{error}")
+
+def kargo_sorgula(kargo_veri):
+    kod=input("Takip Numarsını Giriniz: (Örn:TR101)").strip().upper()
+
+    try:
+        bilgi=kargo_veri["kargolar"][kod]
+        print(f"""
+        Kargo Bilgisi {kod}
+
+        Alıcı: {bilgi["alici"]}
+
+        Ürün: {bilgi["urun"]}
+
+        Durum: {bilgi["durum"]}
+
+        Ağırlık: {bilgi["agirilik_kg"]}
+        
+        
+        """)
+
+    except KeyError:
+
+        print(f" Hata: {kod} numaralı bir kargo kaydı sistemde bulunamadı")
+        
+def yeni_kargo_ekle(kargo_veri):
+
+    print("Yeni Kargo Girişi")
+    kod=input("Takip Bumarası Belirleyin (Örn TR103)").strip().upper()
+    
+    if kod in kargo_veri["kargolar"]:
+        print("Bu takip numarası zaten sistemde kayıtlı")
+        return
+
+    alici=input("Alıcı Ad Soyad").strip()
+    urun=input("Ürün Açıklaması").strip()
+    
+
+    while True:
+        try:
+            agirilik=float(input("Kargo Ağırlığını Giriniz KG: Örn:1.5):"))
+
+            if agirilik<=0:
+                print("Ağırlık 0 veya negatif olamaz Tekrar Deneyiniz")
+                continue
+            break
+
+        except ValueError:
+            print("Hata: Lütfen ağırlığı sadece sayı/ondalıklı olarak giriniz (Örn 1.5)")
+
+    kargo_veri["kargolar"][kod]={
+        "alici":alici,
+        "urun":urun,
+        "durum":"Kargo Alındı",
+        "agirilik_kg":agirilik
+    }
+
+    verileri_kaydet(kargo_veri)
+    print(f"{kod} takip numaralı kargo başarılı bir şekilde oluşturuldu.")
 
 
 verileri_yukle()
